db923b170738043031cb6bdd1c7085438b4011b0
alex289 <mccr4fthd@gmail.com>
2024-08-10 10:54:49 +0200

    chore: Formatting

--- 02-backend-api/middleware/logging.go
+++ 02-backend-api/middleware/logging.go
@@ -22,7 +22,7 @@ func Logging(next http.Handler) http.Handler {
 
 		wrapped := &wrappedWriter{
 			ResponseWriter: w,
-			statusCode: http.StatusOK,
+			statusCode:     http.StatusOK,
 		}
 
 		next.ServeHTTP(wrapped, r)
--- 02-backend-api/middleware/middleware.go
+++ 02-backend-api/middleware/middleware.go
@@ -6,7 +6,7 @@ type Middleware func(http.Handler) http.Handler
 
 func CreateStack(xs ...Middleware) Middleware {
 	return func(next http.Handler) http.Handler {
-		for i := len(xs)-1; i >= 0; i-- {
+		for i := len(xs) - 1; i >= 0; i-- {
 			x := xs[i]
 			next = x(next)
 		}
